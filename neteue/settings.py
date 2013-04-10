27a3c4aa9cda5866544890d32ac4d8b3ba30b5a8
lovesnow <houshao55@gmail.com>
2013-04-10 12:21:44 +0800

    add allow host.

--- neteue/settings.py
+++ neteue/settings.py
@@ -1,4 +1,24 @@
-# Django settings for neteue project.
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2013 Deepin, Inc.
+#               2011 ~ 2013 Hou ShaoHui
+# 
+# Author:     Hou ShaoHui <houshao55@gmail.com>
+# Maintainer: Hou ShaoHui <houshao55@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from common import get_path
 
@@ -220,6 +240,8 @@ GOOGLE_SIMPLE_API_KEY = ''
 GOOGLE_SEARCH_ENGINE_UNIQUE_ID = ''
 GOOGLE_CUSTOM_SEARCH_ENDPOINT = 'https://www.googleapis.com/customsearch/v1'
 GOOGLE_URL_SHORTENER_ENDPOINT = 'https://www.googleapis.com/urlshortener/v1/url'
+
+ALLOWED_HOSTS = ["*",] # 部署时允许所有来源访问
 
 #local settings
 try:
